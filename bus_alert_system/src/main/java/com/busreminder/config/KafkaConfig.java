2baef70a89bcca04cba0b44fde9adbf3faaf655c
ashleysamuel7 <ashleysamuelp@gmail.com>
2026-01-05 08:27:16 +0530

    twilio configuration update afer subscription

--- bus_alert_system/src/main/java/com/busreminder/config/KafkaConfig.java
+++ bus_alert_system/src/main/java/com/busreminder/config/KafkaConfig.java
@@ -1,6 +1,8 @@
 package com.busreminder.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -8,6 +10,11 @@ public class KafkaConfig {
     
     @Value("${kafka.topic.bus-location-updates}")
     private String busLocationUpdatesTopic;
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 
     public String getBusLocationUpdatesTopic() {
         return busLocationUpdatesTopic;
